4e797197e4bf1dda078ba5330b463c35319129d6
aliskar99 <33749128+aliskar99@users.noreply.github.com>
2017-12-11 19:40:26 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,10 +7,10 @@
 	<?php
 		$n=10;
 		$citta="bergamo";
-		$richiamataiesta="pizzeria";
-		# questo script chiamataiama un'API e la inserisce in una tabella 
-		# Indirizzo dell'API da richiamataiedere
-		$indirizzo_pagina="https://api.foursquare.com/v2/venues/searchiamata?v=20161016&richiamataiestary=$richiamataiesta&nit=$n&intent=chiamataeckin&client_id=XTT32MSNDEPOVNS4SCCQEEHT4JJXB3P12AXWR50GNM4KUN1Q&client_secret=FIWH4MQRDDMZH4ZDWFD2XXAEXXTJXMN1SMMFRARQAKBX2L5N&near=$citta";
+		$richiesta="pizzeria";
+		# questo script chiama un'API e la inserisce in una tabella 
+		# Indirizzo dell'API da richiedere
+		$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n&intent=checkin&client_id=XTT32MSNDEPOVNS4SCCQEEHT4JJXB3P12AXWR50GNM4KUN1Q&client_secret=FIWH4MQRDDMZH4ZDWFD2XXAEXXTJXMN1SMMFRARQAKBX2L5N&near=$citta";
 		# Codice di utilizzo di cURL
 		# chiamataiama l'API e la immagazzina in $json
 		$chiamata = curl_init() or die(curl_error());
