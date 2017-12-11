819167e2a1eb688eef5bbcaf24b86618d2975538
aliskar99 <33749128+aliskar99@users.noreply.github.com>
2017-12-11 20:00:33 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -12,14 +12,13 @@
 		# Indirizzo dell'API da richiedere
 		$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n&intent=checkin&client_id=XTT32MSNDEPOVNS4SCCQEEHT4JJXB3P12AXWR50GNM4KUN1Q&client_secret=FIWH4MQRDDMZH4ZDWFD2XXAEXXTJXMN1SMMFRARQAKBX2L5N&near=$citta";
 		# Codice di utilizzo di cURL
-		# chiamataiama l'API e la immagazzina in $json
+		# chiama l'API e la immagazzina in $json
 		$chiamata = curl_init() or die(curl_error());
 		curl_setopt($chiamata, CURLOPT_URL,$indirizzo_pagina);
 		curl_setopt($chiamata, CURLOPT_RETURNTRANSFER, 1);
 		$json=curl_exec($chiamata) or die(curl_error());
 		# Decodifico la stringa json e la salvo nella variabile $data
 		$data = json_decode($json);
-		# Stampa della tabella delle pizzerie.
 		echo "<table>";
 			echo "<tr>";
 				echo "<th>PIZZERIA</th>";
@@ -41,24 +40,8 @@
 				echo "</tr>";
 			}
 		echo "</table>";
-		# Stampa di eventuali errori
 		echo curl_error($chiamata);
 		curl_close($chiamata);
-
-	/*	echo "<form id='forma' method='post' onsubmit='return controllo_campi();'><br/>";
-		echo "<table>";
-		echo "<tr>";
-		echo " <td>Numero elementi (1-50): </td><td><input type='text' value='$n' name='n' id='n' /></td>";
-		echo "</tr>";
-		echo "<tr>";
-		echo " <td>citta: </td><td><input type='text' value='$citta' name='citta' id='citta' /></td>";
-		echo "</tr>";
-		echo "<tr>";
-		echo " <td>Cosa stai cercando?: </td><td><input type='text' value='$richiamataiesta' name='richiamataiesta' id='richiamataiesta' /></td><br/>";
-		echo "</tr>";
-		echo "</table>";
-		echo " <input type='submit' value='Aggiorna tabella' class='btn'/>";
-		echo "</form>";*/
 	?>
 	</body>
 </html>
