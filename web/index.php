0e98bd5e804942f4518e138676ea2763c9ef99ea
aliskar99 <33749128+aliskar99@users.noreply.github.com>
2017-12-12 11:27:24 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -8,14 +8,14 @@
 		$n=20;
 		$citta="bergamo";
 		$richiesta="pizzeria";
-		$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n&intent=checkin&client_id=XTT32MSNDEPOVNS4SCCQEEHT4JJXB3P12AXWR50GNM4KUN1Q&client_secret=FIWH4MQRDDMZH4ZDWFD2XXAEXXTJXMN1SMMFRARQAKBX2L5N&near=$citta";
+		$indirizzo_pagina="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n/intent=checkin&client_id=XTT32MSNDEPOVNS4SCCQEEHT4JJXB3P12AXWR50GNM4KUN1Q&client_secret=FIWH4MQRDDMZH4ZDWFD2XXAEXXTJXMN1SMMFRARQAKBX2L5N&near=$citta";
 		$chiamata = curl_init() or die(curl_error());
 		curl_setopt($chiamata, CURLOPT_URL,$indirizzo_pagina);
 		curl_setopt($chiamata, CURLOPT_RETURNTRANSFER, 1);
 		$json=curl_exec($chiamata) or die(curl_error());
 		$data = json_decode($json);
 		echo("<h1>PIZZERIE A BERGAMO</h1>");
-		echo("<div><table>");
+		echo("<table>");
 		echo("<tr>");
 		echo("<th>PIZZERIA</th>");
 		echo("<th>LATITUDINE</th>");
@@ -34,7 +34,7 @@
 			echo("</td>");
 			echo("</tr>");
 		}
-		echo("</table></div>");
+		echo("</table>");
 		echo curl_error($chiamata);
 		curl_close($chiamata);
 	?>
